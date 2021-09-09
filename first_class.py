0d6c7e8327607db1e5ed9d9beef683ed5dfc58fa
sirdesmond09 <sirdesmond09@gmail.com>
2021-09-09 15:12:42 +0100

    initial commit

---
+++ first_class.py
@@ -0,0 +1,30 @@
+print("Hello world"); print('that')
+print("fff") #this is a comment
+
+print("""Hello, 
+
+This is a string and I love this. 
+
+            Warm regards,
+            Desmond""")
+
+
+print("Hello world")
+
+print("They boy's dad")
+
+AGE = 10
+
+print(AGE)
+
+AGE = 15
+print(AGE)
+
+boy_age  = "45"
+
+True; False
+
+10.04; 120.45
+
+40;33
+
---
+++ second_class.py
@@ -0,0 +1,62 @@
+# welcome = 5
+
+# my_int = 40
+# my_float = 50.43
+# my_string = "60"
+# my_bool = True
+
+# type_of_data = type(my_string) 
+# print(type_of_data)
+
+
+# print(type(my_int))
+
+
+# my_list = ['a boy', 45, True, 45, ["Name", "John", "Mercy",]]
+
+# print(type(my_list))
+
+# a= 3
+# b = 6
+# c=3
+
+# numerator = -b+(b**2 - 4*a*c)**0.5
+# denominator = 2*a
+
+# x = numerator/denominator
+# print(x)
+
+
+# print((5**0.5 + 2)/(15**(4**0.5)))
+
+# print(a is not c)
+
+
+# p = 10000; n=12; r=0.08; t=5; 
+
+# a = p*((1+r/n)**(n*t))
+
+# print(a)
+
+a = 4; b=9; c=8
+
+s = a+b+c
+
+area = (s*(s-a)*(s-b)*(s-c))**0.5
+print(area)
+
+
+man_age = 47 + 10
+
+daughter = man_age//2
+print(daughter)
+
+
+income = 100000
+
+saving_per_month = income * 0.1
+total = saving_per_month *12
+
+print(total)
+
+
