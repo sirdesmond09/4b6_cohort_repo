fa0c563081ab6ff05debeecf250f46ed1ecfdf71
sirdesmond09 <sirdesmond09@gmail.com>
2021-09-14 10:30:51 +0100

    add makeup class

---
+++ makeup_class.py
@@ -0,0 +1,23 @@
+# a = 3
+# b = 2
+# c = 9
+
+
+# numerator = -b+ (b**2 - 4*a*c)**0.5
+# denominator = 2*a
+
+# x = numerator/denominator
+
+# print(x)
+
+
+income=100000
+
+actual_saving = (10/100)*income
+yearly_saving = actual_saving*12
+print(yearly_saving)
+
+
+man_age = 40 + 5
+boy_age = man_age/2.5
+print(boy_age)
